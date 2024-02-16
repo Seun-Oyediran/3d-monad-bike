ec1b348ae65ebfbb94aaa1932187ddb3a6cb2614
Seun Oyediran <seunoyediran22517@gmail.com>
2024-02-16 10:13:20 +0100

    loader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import { Suspense } from "react";
+import { Fragment, Suspense } from "react";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/shared";
+import { Loader } from "@react-three/drei";
 
 function App() {
   return (
-    <Canvas
-      shadows
-      dpr={[1, 2]}
-      camera={{ fov: 60, near: 0.1, far: 1000, position: [0, 1, 10] }}
-    >
-      {/* <color attach="background" args={["#202020"]} />
-  <fog attach="fog" args={["#202020", 5, 20]} /> */}
-      <Suspense fallback={null}>
-        <Experience />
-      </Suspense>
-      {/* <OrbitControls /> */}
-    </Canvas>
+    <Fragment>
+      <Canvas
+        shadows
+        dpr={[1, 2]}
+        camera={{ fov: 60, near: 0.1, far: 1000, position: [0, 1, 10] }}
+      >
+        <Suspense fallback={null}>
+          <Experience />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </Fragment>
   );
 }
 
